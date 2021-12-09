6c9012cd80d67dadfe935ca460aaa3b4e0018732
PRATIK-BOTHRA <74529643+PRATIK-BOTHRA@users.noreply.github.com>
2021-12-09 10:43:05 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+ export const firebaseConfig = {
+  apiKey: "AIzaSyDO0MoWv1r0oS8HJrujaSh-8XHnJiGnW7I",
+  authDomain: "audiobook-14d7f.firebaseapp.com",
+  databaseURL: "https://audiobook-14d7f-default-rtdb.firebaseio.com",
+  projectId: "audiobook-14d7f",
+  storageBucket: "audiobook-14d7f.appspot.com",
+  messagingSenderId: "410479173510",
+  appId: "1:410479173510:web:8d1a9119aa5da2caec1607",
+  measurementId: "G-F3M6P67DF6"
+};
